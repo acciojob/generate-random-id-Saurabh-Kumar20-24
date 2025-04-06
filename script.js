7fb400b97ae7ed654158be747a109bdadc8ebcb9
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-06 16:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let char_list="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321";
+	let res="";
+	for(let i=0; i<l; i++){
+		res+=char_list.charAt(Math.floor(Math.random()*char_list.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
